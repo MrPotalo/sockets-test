96e7cb8b770e842cd77e2643d1b1293e0ccd3dee
Malachi Grant <malachimgrant@gmail.com>
2019-01-28 22:56:10 -0400

    Much progress towards working game

--- client/src/Game.js
+++ client/src/Game.js
@@ -9,14 +9,106 @@ class Game extends Component {
         props.Socket.on('join', ({socketId}) => {
             this.setState({id: socketId});
         });
+        props.Socket.on('joinFailed', () => {
+            alert('oh no');
+        })
+        props.Socket.on('sendState', (obj) => {
+            this.setState(obj);
+        });
 
         this.state = {};
     }
 
+    codeEntered = (e) => {
+        const code = document.getElementById('textCode').value;
+        this.props.Socket.emit('joinRoom', code);
+    }
+
+    startGame = (e) => {
+        this.props.Socket.emit('startGame');
+    }
+
+    placeBet = (e) => {
+        let bet = document.getElementById('txtBet').value;
+        this.props.Socket.emit('placeBet', bet);
+    }
+
+    canClick = true;
+    clickCard(e, card, i) {
+        if (this.canClick) {
+            this.canClick = false;
+            this.props.Socket.emit('playCard', card, i, () => {
+                this.canClick = true;
+            });
+        }
+    }
+
     render() {
+        let game = null;
+        let controlPanel = null;
+        if (this.state.admin) {
+            controlPanel = 
+            <div id='controlPanel'>
+                <input type='button' value='Start Game' onClick={this.startGame}></input>
+            </div>
+        }
+        if (this.state.room) {
+            let meIndex = _.findIndex(this.state.players, {socketId: this.props.Socket.id});
+            let me = this.state.players[meIndex];
+            let cards = null;
+            let gameStage = null;
+            let msg = null;
+            if (this.state.msg) {
+                msg = <div id='tempMsg'>
+                    {this.state.msg}
+                </div>
+            }
+            if (this.state.betting === meIndex) {
+                gameStage = (<div id='gameInfo'>
+                    <input id='txtBet' type='text' placeholder='Input bet'></input>
+                    <input type='button' value='Bet' onClick={this.placeBet}></input>
+                </div>)
+            } else if (this.state.playing === meIndex) {
+                gameStage = (<div id='gameInfo'>
+                    {'Play a card'}
+                </div>)
+            }
+            if (me && me.cards) {
+                cards = <div id='myCards'>
+                    {me.cards.map((card, i) => {
+                        return <div key={i} onClick={this.state.playing === meIndex ? (e) => this.clickCard(e, card, i) : undefined}>{card}</div>
+                    })}
+                </div>
+            }
+            game = <div id="gameScreen">
+                {controlPanel}
+                {cards}
+                {msg}
+                {gameStage}
+                {(this.state.trump ? <div id='trump'>{this.state.trump}</div> : null)}
+                {(this.state.playStack ? <div id='cardStack'>
+                    {this.state.playStack.map((card) => {
+                        return <div>{card}</div>;
+                    })}    
+                </div> : null)}
+                <div id='playerList'>
+                    {this.state.players.map((player) => {
+                        return (<div style={{color: player.socketId === this.props.Socket.id ? 'red' : 'black'}}>{player.name}</div>);
+                    })}
+                </div>
+                <div id='scoreSheet'>
+                    
+                </div>
+            </div>
+        }
         return (
-            <div id="gameScreen">
-                
+            <div id="joinScreen">
+                {game ? game : 
+                <div>
+                    <input id='textCode' type='text' placeholder='Code'></input>
+                    <input type='button' value='Join Game' onClick={this.codeEntered}></input>
+                </div>
+                }
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -12,13 +12,135 @@ app.get('/', (req,res,next) => {
     res.sendFile(__dirname + './index.html');
 });
 
+let rooms = {};
+let socketMap = {};
+
+function generateDeck() {
+    let deck = [];
+    for (let i=0;i<4;i++) {
+        let suit;
+        if (i === 0) {
+            suit = 'S';
+        } else if (i === 1) {
+            suit = 'C';
+        } else if (i === 2) {
+            suit = 'H';
+        } else {
+            suit = 'D';
+        }
+        for (let j = 1; j < 14; j++) {
+            let value;
+            if (j === 1) {
+                value = 'A';
+            } else if (j > 1 && j < 10) {
+                value = j;
+            } else if (j === 10) {
+                value = 'T';
+            } else if (j === 11) {
+                value = 'J';
+            } else if (j === 12) {
+                value = 'Q';
+            } else {
+                value = 'K';
+            }
+            deck.push(suit + value);
+        }
+    }
+    for (let i = 0; i < 4; i++) {
+        deck.push('WZ');
+        deck.push('JE');
+    }
+    deck = _.shuffle(deck);
+    return deck;
+}
+
+function startRound(room, round) {
+    if (rooms[room]) {
+        rooms[room].round = round;
+        rooms[room].betting = round % rooms[room].players.length;
+        let deck = generateDeck();
+        for (let i = 0; i < rooms[room].players.length; i++) {
+            rooms[room].players[i].cards = deck.splice(0, round);
+        }
+        rooms[room].trump = deck.splice(0, 1);
+        io.to(room).emit('sendState', {admin: false, ...rooms[room]});
+    }
+}
 
 io.on('connection', (socket) => {
+    socketMap[socket.id] = socket;
     socket.emit('join', {socketId: socket.id});
-
+    let room = null;
+    socket.on('joinRoom', (code) => {
+        if (rooms[room] && rooms[room].round) {
+            socket.emit('joinFailed');
+            return;
+        }
+        socket.join(code);
+        room = code;
+        let admin = false;
+        if (!rooms[code]) {
+            rooms[code] = {players: [{name: '1', socketId: socket.id}]};
+            admin = true;
+        } else {
+            rooms[code].players.push({name: rooms[code].players.length + 1, socketId: socket.id});
+        }
+        io.to(code).emit('sendState', {room, admin: true, players: rooms[code].players});
+        
+    });
+    socket.on('startGame', () => {
+        startRound(room, 1);
+    });
+    socket.on('placeBet', (bet) => {
+        rooms[room].players[rooms[room].betting].bet = bet;
+        rooms[room].betting++;
+        if (rooms[room].betting >= rooms[room].players.length) {
+            rooms[room].betting = 0;
+        }
+        if (rooms[room].betting === rooms[room].round % rooms[room].players.length) {
+            rooms[room].betting = null;
+            rooms[room].playing = rooms[room].round % rooms[room].players.length;
+        }
+        io.to(room).emit('sendState', rooms[room]);
+    });
+    socket.on('playCard', (card, i, fn) => {
+        rooms[room].players[rooms[room].playing].cards.splice(i, 1);
+        if (!rooms[room].playStack) {
+            rooms[room].playStack = [];
+        }
+        rooms[room].playStack.push(card);
+        rooms[room].playing++;
+        if (rooms[room].playing >= rooms[room].players.length) {
+            rooms[room].playing = 0;
+        }
+        if (rooms[room].playing === rooms[room].round % rooms[room].players.length) {
+            io.to(room).emit('sendState', {msg: 'Woo!', ...rooms[room]});
+            setTimeout(() => {
+                io.to(room).emit('sendState', {msg: null});
+                // determine who took the hand
+                rooms[room].playStack = [];
+                if (rooms[room].players[0].cards.length === 0) {
+                    rooms[room].playing = null;
+                    if (rooms[room].round === 10) {
+                        rooms[room] = undefined;
+                        io.to(room).emit('sendState', {playStack: null, trump: null, room: null, players: null, playing: null, betting: null, round: null});
+                        fn();
+                        return;
+                    } else {
+                        startRound(room, rooms[room].round + 1);
+                        fn();
+                        return;
+                    }
+                }
+                fn();
+            }, 1000);
+        } else {
+            fn();
+            io.to(room).emit('sendState', rooms[room]);
+        }
+    });
     socket.on('disconnect', () => {
-
+        delete socketMap[socket.id];
     })
 });
-
 server.listen(port);
