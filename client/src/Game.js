e5a3fb1f43d8c48e19232026287c602c2dc53233
Malachi Grant <malachimgrant@gmail.com>
2019-03-24 01:55:13 -0300

    TODO: Implement guessing

--- client/src/Game.js
+++ client/src/Game.js
@@ -10,15 +10,137 @@ class Game extends Component {
         props.Socket.on('join', ({socketId}) => {
             this.setState({id: socketId});
         });
+        props.Socket.on("dataChanged", (data) => {
+            this.setState({gameData: data});
+        })
+        props.Socket.on("gameStart", () => {
+            this.setState({stage: "answer"});
+        })
+        props.Socket.on("gameStageGuess", (index) => {
+            this.setState({stage: "guess", guessingIndex: index});
+        })
 
-        this.state = {};
+        this.state = {screen: "start", stage: "start", playerData: {code: "", name: ""}};
+    }
+
+    onChange = (e) => {
+        const val = e.target.value;
+        const index = e.target.dataset.index;
+        this.setState((oldState) => {
+            let newState = _.clone(oldState);
+            _.set(newState, index, val);
+            return newState;
+        })
+    }
+
+    hostGame = (e) => {
+        this.props.Socket.emit("createGame", (roomCode) => {
+            this.setState({screen: "hosting", roomCode});
+        })
+    }
+    startGame = (e) => {
+        this.props.Socket.emit("startGame", this.state.roomCode);
+    }
+
+
+    attemptJoin = (e) => {
+        this.setState({waiting: true});
+        this.props.Socket.emit("requestJoin", this.state.playerData, (err) => {
+            this.setState({waiting: undefined});
+            if (!err) {
+                this.setState({screen: "mainGame"});
+            } else {
+                alert(err.Message);
+            }
+        })
+    }
+    submitAnswer = (e) => {
+        this.props.Socket.emit("submitAnswer", this.state.answer);
     }
 
     render() {
-        return (
-            <div id="gameScreen">
-            </div>
-        )
+        let content = null;
+        switch (this.state.screen) {
+            case "hosting":
+                let innerContent = null;
+                switch (this.state.stage) {
+                    case "answer":
+                        innerContent = (
+                            [<span>Players are answering.</span>,
+                            <div style={{display: "flex", flexDirection: "column", height: "80%", position: "absolute", left: "50px", top: "50px"}}>
+                                {this.state.gameData.answers.map((answer, i) => {
+                                    return <div key={i}>{answer}</div>
+                                })}
+                            </div>]
+                        );
+                        break;
+                    case "guess":
+                            innerContent = (
+                                <span>{"Player " + this.state.gameData.players[this.state.guessingIndex].name + " is guessing."}</span>
+                            )
+                            break;
+                    case "start":
+                    default:
+                            innerContent = (
+                                <input onClick={this.startGame} style={{marginTop: "50px"}} type="button" value="Start game"></input>
+                            ) 
+                }
+                content = (
+                    <div>
+                        <span style={{fontSize: "24pt"}}>{"Code: " + this.state.roomCode}</span>
+                        <br/>
+                        {innerContent}
+                    </div>
+                );
+                break;
+            case "mainGame":
+                    content = (
+                        <div>
+                            {this.state.stage == "answer" ? 
+                            [<span>Enter your answer: </span>,<input onChange={this.onChange} data-index="answer" value={this.state.answer} type="text"></input>,
+                            <br/>,
+                            <input onClick={this.submitAnswer} type="button" value="Submit"></input>]
+                            :
+                            null}
+                            <div id="playerList" style={{position: "absolute", bottom: "0px",width: "100%", height: "40px", display: "flex", flexDirection: "row"}}>
+                                {_.map(_.get(this.state, "gameData.players", []), (player, i) => {
+                                    return <div key={i} style={{border: "1px solid black", height: "100%", width: "100%"}}>{player.name}</div>
+                                })}
+                            </div>
+                        </div>
+                    );
+                    break;
+            case "start":
+            default:
+                content = (
+                    <div>
+                        
+                        {this.state.joinGame ? 
+                            <div>
+                                <span>Room code: </span>
+                                <input id="txtCode" data-index={"playerData.code"} value={this.state.playerData.code} onChange={this.onChange} type="text" placeholder="CODE"></input>
+                                <br/>
+                                <span>Your name: </span>
+                                <input id="txtName" data-index={"playerData.name"} value={this.state.playerData.name} onChange={this.onChange} type="text" placeholder="NAME"></input>
+                                <br/>
+                                <input onClick={this.attemptJoin} id="btnFinalJoin" type="button" value="Join"></input>
+                                {this.state.waiting ? 
+                                    <div>Waiting...</div>
+                                    :
+                                    null
+                                }
+                            </div>
+                            :
+                            <div>
+                                <input id="btnJoinGame" style={{position: "absolute",left: 0,border: "none", backgroundColor: "transparent"}} onClick={this.hostGame} type="button" value="Host Game"></input>
+                                <input onClick={(e) => this.setState({joinGame: true})} type="button" value="Join Game"></input>
+                            </div>}
+                    </div>
+                );
+        }
+        return <div id="mainScreen">
+            {content}
+        </div>
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
+const codes = ["POTO", "HODO", "HODY", "BRSL", "NIPL", "TATO", "TRDR", "LOLZ", "TURD", "POOP", "PODO", "HODI", "PODI", "TATR"]
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -12,9 +13,61 @@ app.get('/', (req,res,next) => {
     res.sendFile(__dirname + './index.html');
 });
 
+let data = {};
+function getClientData(room) {
+    let tempData = _.clone(data[room]);
+    delete tempData.host;
+    for (let i=0;i<tempData.players.length;i++) {
+        delete tempData.players[i].socket;
+    }
+}
+
 io.on('connection', (socket) => {
     socket.emit('join', {socketId: socket.id});
-    
+    let room = null;
+    let playerIndex = -1;
+    socket.on("createGame", (cbFn) => {
+        const roomCode = codes[Math.floor(Math.random() * codes.length)];
+        data[roomCode] = {players: [], host: socket};
+        room = roomCode;
+        socket.join(room);
+        cbFn(roomCode);
+    })
+    socket.on("startGame", () => {
+        io.to(room).emit("gameStart");
+        data[room].answers = [];
+        data[room].numLeftToSubmit = data[room].players.length;
+    });
+    socket.on("submitAnswer", (answer) => {
+        data[room].answers[playerIndex] = answer;
+        if (--data[room].numLeftToSubmit <= 0) {
+            let tempData = getClientData(room);
+            io.to(roomCode).emit("dataChanged", tempData);
+            io.to(room).emit("gameStageGuess", 0);
+        }
+    })
+
+
+    socket.on("requestJoin", (playerData, cbFn) => {
+        const roomCode = playerData.code;
+        if (data[roomCode]) {
+            for (let i=0;i<data[roomCode].players.length;i++) {
+                if (data[roomCode].players[i].name == playerData.name) {
+                    cbFn({Message: "A player with that name is already in this room!"});
+                    return;
+                }
+            }
+            room = roomCode;
+            playerIndex = data[roomCode].players.length;
+            data[roomCode].players.push({name: playerData.name, socket});
+            socket.join(roomCode);
+            cbFn();
+            let tempData = getClientData(room);
+            io.to(roomCode).emit("dataChanged", tempData);
+        } else {
+            cbFn({Message: "Room does not exist!"});
+        }
+    })
     socket.on('disconnect', () => {
         
     })
